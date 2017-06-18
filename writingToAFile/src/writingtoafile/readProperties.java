08a2d7d1c6fc44a4b2678114b1335726814dfa71
Cody <Cody@DESKTOP-2K7A9N4>
2017-06-18 13:17:55 -0400

    class for properties / persistent variables

---
+++ writingToAFile/src/writingtoafile/readProperties.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package writingtoafile;
+import java.io.*;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ *
+ * @author Cody
+ */
+public class readProperties {
+    
+    Properties defProps = new Properties();
+    Properties appProps = new Properties();
+    
+    public void loadProperties() {
+
+        
+        try (FileInputStream in = new FileInputStream("props")) {
+            defProps.load(in);
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
+        
+
+        
+        try (FileInputStream in = new FileInputStream("appProps")) {
+            appProps.load(in);
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
+        
+        
+    }
+    
+    public void saveProperties() {
+        try (FileOutputStream newProperties = new FileOutputStream("appProperties")) {
+            appProps.store(newProperties, "--no comment--");
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
+    }
+    
+    public void getProperties(String property) { //used only for default path for now
+        appProps.getProperty(property);
+    }
+    public void setProperties(String property, String value) {
+        appProps.setProperty(property, value);
+    }
+    public void resetProperties(String property) {
+        appProps.remove(property); // ---- NEEDS TESTING
+    }
+}
